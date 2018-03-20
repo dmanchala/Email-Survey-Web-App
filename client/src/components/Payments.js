a7fd6378d570db74e9db64e2886b97322460f21e
DhruvManchala <dmanchala@gmail.com>
2018-03-20 20:07:09 +0530

    implemented SendGrid functionality

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -13,7 +13,9 @@ class Payments extends Component {
         token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
-        <button className="btn z-depth-0">Add Credits</button>
+        <button className="btn-flat" style={{ color: 'white' }}>
+          Add Credits
+        </button>
       </StripeCheckout>
     );
   }
